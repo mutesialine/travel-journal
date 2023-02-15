1f99733a141c63106ea32f79815ca37e772fb34e
Mutesi <ktesialine@gmail.com>
2023-02-15 19:29:04 +0200

    feat: add data in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,13 @@ export default function Card({image,startingDate,endingDate,link,title,subtitle,
   return (
     <div className="flex justify-between gap-4 px-8 pt-8">
       <div>
-        <img src={image} alt="picture" className="w-[500px]" />
+        <img src={image} alt="picture" className="min-w-[150px]" />
       </div>
       <div className="space-y-1">
         <div className=" flex gap-2">
           <img src="/images/Fill 220.svg" />
           <p>{location}</p>
-          <p>{link}</p>
+         <a href="#" className="underline text-gray-500">{link}</a>
         </div>
 
         <div className="flex font-bold">
